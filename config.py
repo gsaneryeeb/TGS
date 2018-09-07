f614f381d553079fe20448bbed323936c2f6cea2
JasonZ <gsangeryeee@gmail.com>
2018-09-07 11:54:20 +0800

    Debug

--- config.py
+++ config.py
@@ -14,7 +14,8 @@ import json
 from pathlib import Path
 
 
-config = json.loads(open(str(Path('__file__').absolute().parent.parent / 'code' / 'config.json')).read())
+# config = json.loads(open(str(Path('__file__').absolute().parent.parent / 'code' / 'config.json')).read())
+config = json.loads(open('config.json').read())
 
 
 DATA_ROOT = Path(config['input_data_dir']).expanduser()
