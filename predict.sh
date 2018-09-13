0b87f605816bb32d41332a231e0a03e0e27d0f1d
saneryee <gasngeryeee@gmail.com>
2018-09-14 05:41:52 +0800

    added prediction function

--- predict.sh
+++ predict.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e # abort if any command fails
 
+source activate machinelearning
+
 echo "PREDICT"
 echo "---"
 
@@ -20,3 +22,5 @@ python merge.py
 
 #echo "Generate final ensemble"
 #python generate_sub_final_ensemble.py -j=4
+
+source deactivate
