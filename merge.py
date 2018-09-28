dcabe27f60e936e3ce7ded2370a5d5fa8c4ca9f6
saneryee <gasngeryeee@gmail.com>
2018-09-29 07:34:04 +0800

    fix bugs"

--- merge.py
+++ merge.py
@@ -40,7 +40,7 @@ def merge(file_name):
         df = pd.read_csv(df_fold_name, sep=',')
         
         image = df[df['id'].isin([file_name.stem])]
-        print(image['mask']）
+        print(image['mask'])
     #     result[fold] = image['mask']
     #     print('image:',result[fold])
     
